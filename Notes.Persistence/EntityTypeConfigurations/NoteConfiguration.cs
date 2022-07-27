23f360049609acbc7c4b37d3c0140891d05e9ca7
igmo71 <igmo71@mail.ru>
2022-07-27 13:26:48 +0300

    Add project files.

---
+++ Notes.Persistence/EntityTypeConfigurations/NoteConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.Domain;
+
+namespace Notes.Persistence.EntityTypeConfigurations
+{
+    internal class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.Id);
+            builder.Property(x => x.Title).HasMaxLength(250);
+        }
+    }
+}
